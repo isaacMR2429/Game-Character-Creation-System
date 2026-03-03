1ca1af3f2824c5bf6b005c204056fedbabbf7db6
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-03-02 20:46:24 -0800

    put in my fields

--- Game Character Creation System/Character.cs
+++ Game Character Creation System/Character.cs
@@ -8,6 +8,38 @@ namespace Game_Character_Creation_System
 {
     internal class Character
     {
+        // String Fields
+        public string UserName;
+        public string Name;
+        public string Race;
+        public string Class;
+        public string Role;
+        public string Alignment;
+        public string WeaponPreference;
 
+        // Numberic fields
+        public int Level;
+        public int Strength;
+        public int Intelligence;
+        public int Vitality;
+        public int Charisma;
+        public int Dexterity;
+        public int Wisdom;
+        public int Luck;
+        public int CompletedQuests;
+
+        // Boolean Fields
+        public bool IsMagicUser;
+        public bool IsRanged;
+        public bool IsGuildMember;
+
+        // Selection Based Fields
+        public int DifficultyLevel;
+        public int ExperienceLevel;
+
+        // My extra choosen Fields 
+        public string ClassType;
+        public bool HasCompanion;
+        public bool IsCombatReady;
     }
 }

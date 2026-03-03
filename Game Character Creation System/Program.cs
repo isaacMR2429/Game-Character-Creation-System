4eb8b592742f3c8a63ebd58269f924ce35eaaac2
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-03-02 19:56:07 -0800

    Add project files.

---
+++ Game Character Creation System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Game_Character_Creation_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
